4ef5dfcc8696908ac5ce80a7bf4da8184b26991e
Jori Peterson <joripeterson@gmail.com>
2019-10-22 10:28:21 -0600

    Add successful GET request with jquery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax({
+    url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+    success: function(data){
+    $('.word-count').append(`The most popular word is ${Object.keys(data.word)} with a word count of ${Object.values(data.word)}`)
+  },
+    error: function(error) {
+      console.log(error)
+    }
+  })
 })
+
+
+//   function populateWord() {
+//     fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+//     .then(response => response.json())
+//     .then(result => {
+//
+//     })
+//     let word_count = document.getElementById("word-count")
+//       .then(function(data){
+//         console.log(data)
+//         let word = data.results
+//         send(word)
+//     })
+//     .catch(function(error){
+//       console.log(error)
+//     });
+//   }
+// })
